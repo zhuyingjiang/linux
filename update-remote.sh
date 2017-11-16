9e9697b800dd80902e8764ceee2ddead62bbc35b
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2017-11-15 18:58:50 -0600

    cherrypick scripts: initial commit
    add basic script to identify patches in relevant directories for semi-manual
    cherry-pick
    
    run gen_gitlog.sh first, list of SHA1s is stored in gitlog.txt
    (ignore error message fatal: Invalid object name 'ASoC'.)
    then run cherrypick.sh
    
    If this fails,
    1. git cherry-pick --abort;
    2. git rebase --interactive (to this commit)
    3. change gitlog.txt manually with the missing SHA1 and rerun
    cherrypick.sh until things work
    4. update-remote.sh to store result in github branch
    
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>

---
+++ update-remote.sh
@@ -0,0 +1,4 @@
+git push plb :refs/tags/v4.14-backport-latest
+git tag -fa v4.14-backport-latest broonie/for-next
+git push plb backport/v4.14-intel-audio
+git push plb backport/v4.14-intel-audio --tags
