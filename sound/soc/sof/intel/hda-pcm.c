dfd1d3ec274fa58f6e5b0b715d636f9a5e359a9e
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2018-05-15 19:40:52 -0500

    [SQUASHME] ASoC: SOF: Intel: fix HDA PCM rates
    Wrong divisors for 8000 and 9600 Hz, remove non-supported
    frequencies such as 24kHz or 144000 Hz.
    
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>

--- sound/soc/sof/intel/hda-pcm.c
+++ sound/soc/sof/intel/hda-pcm.c
@@ -44,17 +44,15 @@ static inline u32 get_mult_div(struct snd_sof_dev *sdev, int rate)
 {
 	switch (rate) {
 	case 8000:
-		return SDnFMT_DIV(5);
+		return SDnFMT_DIV(6);
 	case 9600:
-		return SDnFMT_DIV(4);
+		return SDnFMT_DIV(5);
 	case 11025:
 		return SDnFMT_BASE(1) | SDnFMT_DIV(4);
 	case 16000:
 		return SDnFMT_DIV(3);
 	case 22050:
 		return SDnFMT_BASE(1) | SDnFMT_DIV(2);
-	case 24000:
-		return SDnFMT_DIV(2);
 	case 32000:
 		return SDnFMT_DIV(3) | SDnFMT_MULT(2);
 	case 44100:
@@ -65,8 +63,6 @@ static inline u32 get_mult_div(struct snd_sof_dev *sdev, int rate)
 		return SDnFMT_BASE(1) | SDnFMT_MULT(2);
 	case 96000:
 		return SDnFMT_MULT(2);
-	case 144000:
-		return SDnFMT_MULT(3);
 	case 176400:
 		return SDnFMT_BASE(1) | SDnFMT_MULT(4);
 	case 192000:
